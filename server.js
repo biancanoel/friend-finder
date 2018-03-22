ad4ed5631685525feec6831037abf26e3dbda1c6
Bianca Torres <biancatorres@Biancas-MacBook.local>
2018-03-21 19:49:10 -0700

    code cleaned up, still needs styling

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 // Dependencies
-// =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
@@ -7,7 +6,6 @@ var routes = require('./routing/htmlRoutes');
 var friends = require('./app/data/friends.js');
 
 // Sets up the Express App
-// =============================================================
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -15,44 +13,11 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//Routes to home page and survey page
+//HTML Routes
 require('./routing/htmlRoutes.js')(app);
 
-////////////////////API ROUTES 
+//API Routes
 require('./routing/apiRoutes.js')(app);
-
-
-// app.get("/friends", function (req, res) {
-//     return res.json(friends);
-// });
-
-// app.post("/api/new", function (req, res) {
-//     var newFriend = req.body;
-  
-//     var scoresInt = [];
-//     newFriend.scores.forEach(function (score) {
-//         scoresInt.push(parseInt(score));
-//     });
-
-//     //Determine best friend match 
-//     var highScore = 1000;
-//     var bestFriend;
-
-//     for (i = 0; i < friends.length; i++) {
-//         var sum = 0;
-//         for (j = 0; j < friends[i].scores.length; j++) {
-//             sum += Math.abs(scoresInt[j] - friends[i].scores[j]);
-//         }
-//         if (sum < highScore) {
-//             highScore = sum;
-//             bestFriend = friends[i];
-//         };
-//     }; 
-
-//     friends.push(newFriend);
-//     return res.json(bestFriend);
-// });
-
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
