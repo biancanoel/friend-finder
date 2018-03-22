5728a156e52ebc3838b675b5a01388b233c44ba0
Bianca Torres <biancatorres@Biancas-MacBook.local>
2018-03-21 19:24:30 -0700

    app is functional, needs styling

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,28 +1,28 @@
 
 var friendsArray = [
     {
-        name: "Bianca",
-        photo: "https://www.nationalgeographic.com/content/dam/animals/pictures/mammals/d/domestic-dog/domestic-dog.adapt.590.1.jpg",
+        name: "Brian",
+        photo: "http://media.comicbook.com/wp-content/uploads/2013/12/brian-griffin-family-guy.jpg",
         scores: [1, 2, 3, 4, 5]
     },
     {
-        name: "Mia",
-        photo: "https://www.nationalgeographic.com/content/dam/animals/pictures/mammals/d/domestic-dog/domestic-dog.adapt.590.1.jpg",
+        name: "Michael Scott",
+        photo: "http://www.success.com/sites/default/files/main/blogposts/michaelscott_2.jpg",
         scores: [5, 4, 3, 2, 1]
     },
     {
         name: "Eleanor Roosevelt",
-        photo: "https://www.nationalgeographic.com/content/dam/animals/pictures/mammals/d/domestic-dog/domestic-dog.adapt.590.1.jpg",
+        photo: "https://www.biography.com/.image/t_share/MTQ1NDY2MDQyODAwNDE2MzEx/eleanor-roosevelt---mini-biography.jpg",
         scores: [3, 3, 2, 3, 1]
     },
     {
         name: "Actual Trash",
-        photo: "https://www.nationalgeographic.com/content/dam/animals/pictures/mammals/d/domestic-dog/domestic-dog.adapt.590.1.jpg",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZ03AYwoCXaDd9Iw5cUy1SXXPRL_kbF9xaQIY1TN8kAw2x6TZk5Q",
         scores: [3, 2, 4, 3, 1]
     },
     {
         name: "Will Ferrel",
-        photo: "https://www.nationalgeographic.com/content/dam/animals/pictures/mammals/d/domestic-dog/domestic-dog.adapt.590.1.jpg",
+        photo: "https://ia.media-imdb.com/images/M/MV5BMTMxMDIzMDEzNF5BMl5BanBnXkFtZTcwODcxMjE2Mg@@._V1_UY317_CR2,0,214,317_AL_.jpg",
         scores: [4, 5, 3, 2, 2]
     },
 
